38f989a26a070720e1d0cc814c53da76f2d56351
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-19 17:45:37 +0200

    intercepted password length

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+//intercettiamo l'arrivo della variabile isset()
+//'?' op. ternario, se arriva il parametro applichiamo intval() else lasciamo  vuoto
+
+$password_length = isset($_GET['length-password']) ? intval($_GET['length-password']) : 0;
+
+var_dump($password_length);
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +18,16 @@
 </head>
 
 <body>
+    <form action="index.php" method="GET">
+        <h1>imposta la lunghezza della password</h1>
+        <label for="length-pasword"></label>
+        <input type="text" id="length-pasword" name="length-password" value="">
+        <button type="submit">invia</button>
+    </form>
+
+    <?php  ?>
+
+
 
 </body>
 
