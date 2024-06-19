d070067a77ff264513d262e4dc57a549afedbd54
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-19 22:21:22 +0200

    [upd] rand.password printed in html

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-//intercettiamo l'arrivo della variabile isset()
-//'?' op. ternario, se arriva il parametro applichiamo intval() else lasciamo  vuoto
-
-$password_length = isset($_GET['length-password']) ? intval($_GET['length-password']) : 0;
 
 //la funzione prenderà come parametro la il valore in GET
 //range caratteri x pass. lunghezza del range. string vuota per new pass
@@ -11,14 +7,31 @@ function password_gen($value)
 {
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     $len_chars = strlen($chars);
-    $password = '';
+    $random_password = '';
 
     for ($i = 0; $i < $value; $i++) {
-        $password .= $chars[rand(0, $len_chars - 1)];
+        $random_password .= $chars[rand(0, $len_chars - 1)];
     }
+
+    return $random_password;
 }
 
+
+//intercettiamo l'arrivo della variabile isset()
+//'?' op. ternario, se arriva il parametro applichiamo intval() else lasciamo  vuoto
+
+$password_length = isset($_GET['length-password']) ? intval($_GET['length-password']) : 0;
+$password_generated = '';
+
+if ($password_length > 0) {
+    $password_generated = password_gen($password_length);
+}
+
+
+
+
 var_dump($password_length);
+var_dump($password_generated);
 ?>
 
 <!DOCTYPE html>
@@ -37,9 +50,14 @@ var_dump($password_length);
         <input type="text" id="length-pasword" name="length-password" value="">
         <button type="submit">invia</button>
     </form>
+    <!--stampiamo il valore-->
+    <?php if ($password_generated) :  ?>
 
-    <?php  ?>
+        <h2>La password generata è:</h2>
 
+        <p> <?php echo $password_generated  ?> </p>
+
+    <?php endif ?>
 
 
 </body>
